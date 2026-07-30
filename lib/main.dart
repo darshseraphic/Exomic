595a104efffbca6e43ea4ae180f877da9936b9b3
darshseraphic <darsh.seraphic@gmail.com>
2026-07-30 20:11:37 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/database.dart';
-import 'navbar.dart';
 import 'features/settings.dart';
 import 'splash.dart'; // Imported your new splash screen
 
